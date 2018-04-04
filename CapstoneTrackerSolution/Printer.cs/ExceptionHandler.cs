e537dd315bc06230a0e79e91cd81141ce49c811e
Ian Effendi <effendiian@gmail.com>
2018-04-03 20:55:43 -0400

    Add stub class of ExceptionHandler.

---
+++ CapstoneTrackerSolution/Printer.cs/ExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿/*
+    ExceptionHandler.cs
+    Handles printing exceptions to files.    
+    ***
+    04/03/2018
+    ISTE 330.02 - Group 16
+    Ian Effendi
+    Jacob Toporoff
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer.cs
+{
+    public class ExceptionHandler
+    {
+        // STUB.
+    }
+}
